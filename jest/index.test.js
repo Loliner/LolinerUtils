f89c2a6856da930e7fa429aa9913a0f16d7a23c8
郑就 <zhengjiu@happyplaygame.net>
2018-03-26 20:51:29 +0800

    init

---
+++ jest/index.test.js
@@ -0,0 +1,59 @@
+
+const L = require('../src/index.js');
+
+const num1 = 1;
+const num2 = 1;
+const num3 = 2;
+
+const str1 = 'a';
+const str2 = 'a';
+const str3 = 'b';
+
+const arr1 = [1, 2, 3];
+const arr2 = [1, 2, 3];
+const arr3 = [1, 2, 3, 4];
+
+const obj1 = { name: 'Loliner' };
+const obj2 = { name: 'Loliner' };
+const obj3 = { name: 'Loliner', name: 20 };
+
+const complexArr1 = [
+    { name: 'Loliner', age: 20, skill: ['javascript', 'python', 'java'] },
+    { name: 'Jack', age: 23, skill: ['cocos2dx', 'unity'] },
+    { name: 'Bob', age: 24, skill: ['php', 'c#', 'java'] },
+];
+const complexArr2 = [
+    { name: 'Loliner', age: 20, skill: ['javascript', 'python', 'java'] },
+    { name: 'Jack', age: 23, skill: ['cocos2dx', 'unity'] },
+    { name: 'Bob', age: 24, skill: ['php', 'c#', 'java'] },
+];
+const complexArr3 = [
+    { name: 'Loliner', age: 20, skill: ['javascript', 'python', 'java'] },
+    { name: 'Jack', age: 23, skill: ['cocos2dx', 'unity'] },
+    { name: 'Bob', age: 24, skill: ['php', 'c#', 'c++'] },
+];
+
+test('compare number', () => {
+    expect(L.deepCompare(num1, num2)).toBe(true);
+    expect(L.deepCompare(num1, num3)).toBe(false);
+});
+
+test('compare string', () => {
+    expect(L.deepCompare(str1, str2)).toBe(true);
+    expect(L.deepCompare(str1, str3)).toBe(false);
+});
+
+test('simple arr', () => {
+    expect(L.deepCompare(arr1, arr2)).toBe(true);
+    expect(L.deepCompare(arr1, arr3)).toBe(false);
+});
+
+test('simple obj', () => {
+    expect(L.deepCompare(obj1, obj2)).toBe(true);
+    expect(L.deepCompare(obj1, obj3)).toBe(false);
+});
+
+test('complex array', ()=> {
+    expect(L.deepCompare(complexArr1, complexArr2)).toBe(true);
+    expect(L.deepCompare(complexArr1, complexArr3)).toBe(false);
+});
---
+++ src/index.js
@@ -0,0 +1,94 @@
+
+
+let Loliner = {
+
+    isObject(target) {
+        return Object.prototype.toString.call(target) === '[object Object]';
+    },
+
+    isArray(target) {
+        return Object.prototype.toString.call(target) === '[object Array]';
+    },
+
+    sameType(origin, target) {
+        return Object.prototype.toString.call(origin) === Object.prototype.toString.call(target);
+    },
+
+    compareCore(origin, target) {
+        if (!Loliner.sameType(origin, target)) {
+            return false;
+        } else if (Loliner.isObject(origin)) {
+            return Loliner.deepCompareObject(origin, target);
+        } else if (Loliner.isArray(origin)) {
+            return Loliner.deepCompareArray(origin, target);
+        } else {
+            return origin === target;
+        }
+    },
+
+    // 数组深度对比
+    deepCompareArray(origin, target) {
+        if (origin && !target || !origin && target) {
+            return false;
+        }
+        if (origin.length !== target.length) {
+            return false;
+        }
+        for (let i = 0; i < origin.length; i++) {
+            let originItem = origin[i];
+            let targetItem = target[i];
+
+            if (!Loliner.compareCore(originItem, targetItem)) {
+                return false;
+            };
+        }
+        return true;
+    },
+
+    // 对象深度对比
+    deepCompareObject(origin, target) {
+        if (origin && !target || !origin && target) {
+            return false;
+        }
+        for (let key of Object.keys(origin)) {
+            let originItem = origin[key];
+            let targetItem = target[key];
+
+            if (!Loliner.compareCore(originItem, targetItem)) {
+                return false;
+            }
+        }
+        return true;
+    },
+
+    // 变量深度对比
+    deepCompare(origin, target) {
+        if (origin && !target || !origin && target) {
+            return false;
+        }
+        if (!Loliner.compareCore(origin, target)) {
+            return false;
+        }
+        return true;
+    },
+
+    formatDate: (date, fmt) => {
+        let o = {
+            "M+": date.getMonth() + 1,               //月份  
+            "d+": date.getDate(),                    //日  
+            "h+": date.getHours(),                   //小时  
+            "m+": date.getMinutes(),                 //分  
+            "s+": date.getSeconds(),                 //秒  
+            "q+": Math.floor((date.getMonth() + 3) / 3), //季度  
+            "S": date.getMilliseconds()              //毫秒  
+        };
+        if (/(y+)/.test(fmt))
+            fmt = fmt.replace(RegExp.$1, (date.getFullYear() + "").substr(4 - RegExp.$1.length));
+        for (let k in o)
+            if (new RegExp("(" + k + ")").test(fmt))
+                fmt = fmt.replace(RegExp.$1, (RegExp.$1.length == 1) ? (o[k]) : (("00" + o[k]).substr(("" + o[k]).length)));
+        return fmt;
+    },
+};
+
+module.exports = Loliner;
