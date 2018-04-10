16a87560a5407b190d786a4d8ad534268fbc3e7d
郑就 <zhengjiu@happyplaygame.net>
2018-04-10 15:45:35 +0800

    countBy and simple every

--- jest/collection.test.js
+++ jest/collection.test.js
@@ -10,3 +10,25 @@ test('count', () => {
         return item > 2;
     })).toEqual(2);
 });
+
+test('countBy', () => {
+    const input1 = [6.1, 4.2, 6.3];
+    const output1 = { '4': 1, '6': 2 };
+    expect(L.countBy(input1, Math.floor)).toEqual(output1);
+
+    const input2 = ['one', 'two', 'three'];
+    const output2 = { '3': 2, '5': 1 };
+    expect(L.countBy(input2, 'length')).toEqual(output2);
+});
+
+test('every', () => {
+    const input = [true, 1, null, 'yes'];
+    expect(L.every(input, Boolean)).toBe(false);
+
+    const users = [
+        { 'user': 'barney', 'age': 36, 'active': false },
+        { 'user': 'fred', 'age': 40, 'active': false }
+    ];
+    const matcher = { 'user': 'barney', 'active': false };
+    expect(L.every(users, matcher)).toEqual(false);
+})
--- src/Collection.js
+++ src/Collection.js
@@ -1,24 +1,66 @@
 
 
 import Helper from './Helper.js';
+import Type from './Type.js'
 
 const Collection = {
-    count(...originParams) {
-        let { params, comparator } = Helper._getComparator(originParams);
-        let arr = params[0];
 
+    // 计数器（没啥卵用）
+    count(collection, comparator) {
         let num = 0;
         if (comparator) {
-            for (let i = 0; i < arr.length; i++) {
-                if (comparator(arr[i])) {
-                    num++;
-                }
+            for (let i = 0; i < collection.length; i++) {
+                comparator(collection[i]) && num++;
             }
         } else {
-            num = arr.length;
+            num = collection.length;
         }
 
         return num;
+    },
+
+    // 聚合计数器（大多数情况用不上啊）
+    countBy(collection, comparator, initializer) {
+        let result = initializer ? initializer() : {};
+        for (let i = 0; i < collection.length; i++) {
+            let item = collection[i];
+            let key;
+            if (Type.isFunction(comparator)) {
+                key = comparator(item);
+            } else {
+                key = item[comparator];
+            }
+            if (Object.hasOwnProperty.call(result, key)) {
+                result[key]++;
+            } else {
+                result[key] = 1;
+            }
+        }
+        return result;
+    },
+
+    every(collection, comparator) {
+        // array
+        if (Type.isFunction(comparator)) {
+            for (let i = 0; i < collection.length; i++) {
+                if (!comparator(collection[i])) {
+                    return false;
+                }
+            }
+            return true;
+        }
+        // object（只支持一层，后续写个contains方法吧，检测对象的涵盖关系）
+        else {
+            for (let i = 0; i < collection.length; i++) {
+                let item = collection[i];
+                for (let key of Object.keys(comparator)) {
+                    if (comparator[key] != item[key]) {
+                        return false;
+                    }
+                }
+            }
+            return true
+        }
     }
 };
 
